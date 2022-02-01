17ffd1f5e1068faac427eb93fabb191a33885c16
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-01 16:28:22 +0530

    configured Post in app.js

--- Newsletter-SignUp/app.js
+++ Newsletter-SignUp/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public"));
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/signup.html');
+})
+
+app.post('/', (req,res)=>{
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+    console.log(fname);
+    console.log(lname);
+    console.log(email);
 })
 
 app.listen(3000, ()=>{
