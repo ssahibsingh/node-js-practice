668a35652b8f9d4de58fa797262ce0722988775a
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-01 19:27:02 +0530

    configured MailChimp API

--- Newsletter-SignUp/app.js
+++ Newsletter-SignUp/app.js
@@ -1,26 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https')
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/signup.html');
 })
 
-app.post('/', (req,res)=>{
-    var fname = req.body.fname;
-    var lname = req.body.lname;
-    var email = req.body.email;
-    console.log(fname);
-    console.log(lname);
-    console.log(email);
+app.post('/', (req, res) => {
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/list/245e9c2e36"
+
+    const options = {
+        method: "POST",
+        auth: "snone181:e7ca67b873d0c66d98666c52e8b3adbb-us14"
+    }
+
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("listening at port 3000");
 })
+
+
+// API Key
+// e7ca67b873d0c66d98666c52e8b3adbb-us14
+
+// List ID
+// 245e9c2e36
