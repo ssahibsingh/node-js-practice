58a8ab265c8e3566d7cb806bff0ee36fa104bc36
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-01 16:20:01 +0530

    SignUp Page setup Done

--- Newsletter-SignUp/app.js
+++ Newsletter-SignUp/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
-
 const app = express();
-app.bodyParser = urlencoded({entended: true});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/signup.html');
+})
 
 app.listen(3000, ()=>{
     console.log("listening at port 3000");
