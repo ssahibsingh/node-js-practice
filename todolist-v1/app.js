bfe57382786b2800343f90c2a712d6773e10ebbf
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-04 22:48:36 +0530

    Added Todo List v1

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+var items = ["Cake", "Bun", "Coffee"];
 app.get('/', (req, res) => {
-    res.send("hello ")
+    var today = new Date();
+    var options = { weekday: "long", day: "numeric", month: "long" }
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render('list', { kindOfDay: day, newItemList: items });
 })
 
-app.listen(3000, ()=>{
+app.post('/', (req, res) => {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+})
+
+app.listen(3000, () => {
     console.log('listening on port 3000');
 })
