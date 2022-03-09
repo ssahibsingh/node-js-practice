8da89c5a9f49bc0610727abe3d4099f446f17e64
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-03-09 22:27:02 +0530

    Update server.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
-
 app.get('/', function(req, res){
     res.send('Hello');
 })
-
 app.get('/about', function(req, res){
     res.send("Hi, I am Sahib Singh");
 })
-
 app.get('/contact', function(req, res){
     res.send("Contact me at snone181@gmail.com");
 })
-
-app.listen(3000, function(){
-    console.log('listening on port 3000');
+app.listen(5000, function(){
+    console.log('listening on port 5000');
 })
